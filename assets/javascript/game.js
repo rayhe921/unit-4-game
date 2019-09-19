8aa30592187338fe49747094bd2bd327d749631b
Ray He <rayhe921@gmail.com>
2019-09-18 22:11:54 -0700

    added crystal button value

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,11 +6,11 @@ var loseCount = 0;
 var random;
 var userScore = 0;
 var crystalNum = [];
-var crystal1 = 0;
-var crystal2 = 0;
-var crystal3 = 0;
-var crystal4 = 0;
 
+// score value
+
+$("winValue").text("Wins: " + winCount);
+$("loseValue").text("Losses: " + loseCount);
 
 // Number Generators
 
@@ -36,27 +36,40 @@ console.log(crystalValue());
 // Buttons
 
 $("#button1").on("click", function () {
+    var crystal1;
     crystal1 = crystalNum[0];
     userScore = userScore + crystal1;
     console.log(userScore);
+    $("#scoreNum").text("User Score: " + userScore);
 });
 
 $("#button2").on("click", function () {
+    var crystal2; 
     crystal2 = crystalNum[1];
     userScore = userScore + crystal2;
     console.log(userScore);
+    $("#scoreNum").text("User Score: " + userScore);
 });
 
 $("#button3").on("click", function () {
+    var crystal3;
     crystal3 = crystalNum[2];
     userScore = userScore + crystal3;
     console.log(userScore);
+    $("#scoreNum").text("User Score: " + userScore);
 });
 
 $("#button4").on("click", function () {
+    var crystal4;
     crystal4 = crystalNum[3];
     userScore = userScore + crystal4;
     console.log(userScore);
+    $("#scoreNum").text("User Score: " + userScore);
 });
 
-console.log(userScore);
+// Win and loss condition
+
+if (userScore === random) {
+    winCount++;
+        
+}
